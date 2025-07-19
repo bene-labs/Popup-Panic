62b5673b307e9f98a1fa225411de447bb7c33c40
Bene <benedikt.mertes@online.de>
2025-07-19 18:58:29 +0200

    implement add images, fix close button

--- nodes/popups/base_window.gd
+++ nodes/popups/base_window.gd
@@ -6,6 +6,8 @@ static var min_size = 0.3
 static var max_size = 1
 
 @export var backgroud : Node2D
+@export var add_textures : Array[Texture2D]
+@export var add_image : TextureRect
 var size := 1.0
 
 static func spawn(difficulty, position):
@@ -13,6 +15,6 @@ static func spawn(difficulty, position):
 	new_popup.size = randf_range(min_size, max_size)
 	new_popup.backgroud.scale = Vector2(new_popup.size, new_popup.size)
 	new_popup.global_position = position
-	
+	new_popup.add_image.texture = new_popup.add_textures.pick_random()
 	return new_popup
 	
